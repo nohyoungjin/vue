833b8615917c76e1081672d895a167e3a281883c
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2022-04-08 16:31:38 +0900

    서버 데이터 바인딩

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> <span>|</span>
+    <router-link to="/about">About</router-link> <span>|</span>
+    <router-link to="/DataBinding">DataBinding</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+nav {
+  font-size: 24px;
+  list-style: none;
+}
+
+nav span {
+  display: inline-block;
+  position: relative;
+  top: -4px;
+  padding: 0 20px 0 10px;
+  font-size: 10px;
+}
+
+a {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ vue-project/src/views/DataBinding.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1>Hello, {{title}}</h1> <br>
+
+    <div :key="i" v-for="(product,i) in productList" style="font-size:40px;">
+        <span>{{product.name}}</span> /
+        <span>{{product.price}} 원</span> /      
+        <span>{{product.category}}</span>    
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+export default {
+    data() {
+        return {
+            productList: []
+        };
+    },
+    created() {
+        this.getList();
+    },
+    methods: {
+        async getList() {
+            this.productList = await this.$api("https://4feb8e42-6956-488b-b41f-6e8d520cd2d6.mock.pstmn.io/list", "get");                                             
+        }
+    }
+}
+
+</script>

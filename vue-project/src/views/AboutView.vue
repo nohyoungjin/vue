075dc808eb760df6fdecada4f526c3f12019758b
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2022-12-15 15:56:30 +0900

    업데이트 3

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -3,19 +3,17 @@
     <div class="p-container">
         <div class="p-container_inner">
 
-            <div class="padded-main">
-                <div id="profile-image-container" class="elevation-4">
-                    <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_newvori_logo.jpg" id="profile-image" alt="">
-                </div>
+            <div id="profile-image-container" class="flex justify-center items-center overflow-hidden relative z-50 w-[200px] h-[200px] mb-[-100px] shadow-lg ring-1 rounded-[14px] translate-y-[-40%]">
+                <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_newvori_logo.jpg" id="profile-image" alt="">
             </div>
 
-            <div class="box-container">
+            <div class="box-container flex pt-[50px] pb-[150px]">
                 <div class="box-left">
                     <h1 class="heading-3 text-3xl font-bold">뉴보리</h1>
                     <a v-bind:href="siteUrl" id="external-link" class="round-button h48" target="_blank" >홈페이지</a>
                 </div>
 
-                <div class="box-right">
+                <div class="box-right flex-1 text-left">
                     <p id="business-headline" class="heading-6">
                         <span>뉴보리는 긍정적인 사회변화를 이끌어가는 영상 채널입니다.</span>
                     </p>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,40 +1,40 @@
 <template>
 
-<div class="p-container">
-  <div class="p-container_inner">
+  <div class="p-container">
+    <div class="p-container_inner">
 
-    <transition
-      appear
-      appear-class="custom-appear"
-      appear-to-class="custom-appear-to"
-      appear-active-class="custom-appear-active"
-    >
-      <div class="flex flex-wrap justify-between items-center">
-        <div class="business-items hover-elevation b1 relative flex-initial w-[30%] mb-[26px] border-1 rounded-lg">
-          <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
-            <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovation/img_sesang_logo.jpg" alt="">
-          </a>
+      <transition
+        appear
+        appear-class="custom-appear"
+        appear-to-class="custom-appear-to"
+        appear-active-class="custom-appear-active"
+      >
+        <div class="flex flex-wrap justify-between items-center">
+          <div class="relative flex-initial w-[30%] mb-[50px] border-1 border-solid border-zinc-200 rounded-lg ease-in-out duration-300 before:table before:pt-[72%] hover:border-zinc-400 max-lg:w-[49%] max-lg:mb-[2%] max-md:w-[100%]">
+            <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
+              <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovation/img_sesang_logo.jpg" alt="">
+            </a>
+          </div>
+          <div class="relative flex-initial w-[30%] mb-[50px] border-1 border-solid border-zinc-200 rounded-lg ease-in-out duration-300 before:table before:pt-[72%] hover:border-zinc-400 max-lg:w-[49%] max-lg:mb-[2%] max-md:w-[100%]">
+            <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
+              <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_sit_logo.jpg" alt="">
+            </a>
+          </div>
+          <div class="relative flex-initial w-[30%] mb-[50px] border-1 border-solid border-zinc-200 rounded-lg ease-in-out duration-300 before:table before:pt-[72%] hover:border-zinc-400 max-lg:w-[49%] max-lg:mb-[2%] max-md:w-[100%]">
+            <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
+              <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_newvori_logo.jpg" alt="">
+            </a>
+          </div>
+          <div class="relative flex-initial w-[30%] mb-[50px] border-1 border-solid border-zinc-200 rounded-lg ease-in-out duration-300 before:table before:pt-[72%] hover:border-zinc-400 max-lg:w-[49%] max-lg:mb-[2%] max-md:w-[100%]">
+            <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
+              <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovation/img_training_logo.jpg" alt="">
+            </a>
+          </div>
         </div>
-        <div class="business-items hover-elevation b2 relative flex-initial w-[30%] mb-[26px] rounded-lg">
-          <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
-            <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_sit_logo.jpg" alt="">
-          </a>
-        </div>
-        <div class="business-items hover-elevation b3 relative flex-initial w-[30%] mb-[26px] rounded-lg">
-          <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
-            <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovator/img_newvori_logo.jpg" alt="">
-          </a>
-        </div>
-        <div class="business-items hover-elevation b4 relative flex-initial w-[30%] mb-[26px] rounded-lg">
-          <a href="javascript:" class="flex justify-center items-center absolute top-0 left-0 w-full h-full">
-            <img src="http://222.236.61.86:8111/SK_HappyAnd/images/page/innovation/img_training_logo.jpg" alt="">
-          </a>
-        </div>
-      </div>
-    </transition>
+      </transition>
 
+    </div>
   </div>
-</div>
 
 </template>
 
@@ -64,21 +64,21 @@ export default {
 
 <style>
 
-.custom-appear-active div.b1 {
+.custom-appear-active div:first-child(1) {
     opacity: 0
 }
 
-.custom-appear-active div.b2 {
+.custom-appear-active div:first-child(2) {
     opacity: 0;
     transform: translateX(50px)
 }
 
-.custom-appear-active div.b3 {
+.custom-appear-active div:first-child(3) {
     opacity: 0;
     transform: translateX(-100px)
 }
 
-.custom-appear-active div.b4 {
+.custom-appear-active div:first-child(4) {
     opacity: 0;
     transform: translateY(150px)
 }

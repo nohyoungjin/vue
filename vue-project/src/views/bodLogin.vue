9dab5b032573a372d39b5069c7d6738ae6071908
nohyoungjin <45346737+nohyoungjin@users.noreply.github.com>
2023-01-10 15:17:09 +0900

    로그인 유효성 검사 2

--- vue-project/src/views/bodLogin.vue
+++ vue-project/src/views/bodLogin.vue
@@ -1,36 +1,55 @@
 <template>
-  <div class="mt-4 -mb-3">
-    <div class="not-prose mb-4">
-      <div class="flex space-x-2">
-        </div></div><div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25"><div class="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]" style="background-position: 10px 10px;"></div><div class="relative rounded-xl overflow-auto">
-  <div class="max-w-sm mx-auto bg-white shadow pt-6 pb-4 px-6">
-     
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+        <!-- <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Or
+          {{ ' ' }}
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+        </p> -->
+      </div>
+
       <Form @submit="login" :validation-schema="schema">
         <Field name="id" type="id" v-slot="{ field, errorMessage, meta }">
-          <div class="field">
-            <label for="id" class="block text-sm font-medium text-slate-700 text-left">아이디</label>
-            <div class="mt-1">
-              <input class="form-control" v-model="user.userid" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
-              <span class="errMsg block pt-2 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
-            </div>
+          <input type="hidden" name="remember" value="true" />
+        <div class="field">
+          <div class="-space-y-px rounded-md shadow-sm">
+            <label for="id" class="sr-only">아이디</label>
+            <input class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="아이디" v-model="user.userid" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
+          </div>
+          <span class="errMsg block pt-2 pl-2 pb-2 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
           </div>
         </Field>
 
         <Field name="password" type="password" v-slot="{ field, errorMessage, meta }">
-          <div class="field mt-3">
-            <label for="pwd" class="block text-sm font-medium text-slate-700 text-left">비밀번호</label>
-            <div class="mt-1">
-              <input type="password" class="form-control" v-model="user.password" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
-              <span class="errMsg block pt-2 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
+          <div class="field">
+            <div class="-space-y-px rounded-md shadow-sm">
+              <label for="pwd" class="sr-only">비밀번호</label>
+              <input type="password" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="비밀번호" v-model="user.password" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
             </div>
+            <span class="errMsg block pt-2 pl-2 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
           </div>
         </Field>
 
         <div class="loginErr" v-if="loginErr">로그인에 실패하였습니다.</div>
-        <button type="submit" class="btn mt-3 mb-3">로그인</button>
+        <div class="mt-3">
+          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" x-description="Heroicon name: mini/lock-closed" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd"></path>
+                </svg>
+            </span>
+            로그인
+          </button>
+        </div>
+
+        <div class="mt-3 text-[14px]"><a href="/signUp">가입하기</a></div>
       </Form>
+
+    </div>
   </div>
-</div><div class="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div></div></div>
 
 </template>
 
@@ -66,7 +85,7 @@ export default {
     schema() {
       return object({
         id: string().required('아이디를 입력해주세요.'),
-        password: string().required('비밀번호를 입력해주세요.').matches(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[#?!@$%^&*-])(?=.{8,})/,'영문자, 숫자, 특수문자를 조합하여 최소8자리를 입력해주세요.')
+        password: string().required('비밀번호를 입력해주세요.').matches(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[#?!@$%^&*-])(?=.{8,})/,'영문자, 숫자, 특수문자를 조합하여 최소 8자리를 입력해주세요.')
       });
     },
   },
--- vue-project/src/views/signUp.vue
+++ vue-project/src/views/signUp.vue
@@ -1,28 +1,56 @@
 <template>
-    <div class="app">
-      <br />
-      <Form @submit="signUp">
-      <div class="input_row">
-        <label for="id">아이디</label>
-        <input type="text" name="id" id="id" v-model="user.userid" />
-      </div>
-      <div class="input_row">
-        <label for="name">이름</label>
-        <input type="text" id="name" v-model="user.name" />
-      </div>
-      <div class="input_row">
-        <label for="password">비밀번호</label>
-        <input type="password" id="password" v-model="user.password" />
-      </div>
-      <button>가입하기</button>
-      <!-- <button @click="signUp">가입하기</button> -->
+  <div class="mt-4 -mb-3">
+    <div class="not-prose mb-4">
+      <div class="flex space-x-2">
+        </div></div><div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25"><div class="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]" style="background-position: 10px 10px;"></div><div class="relative rounded-xl overflow-auto">
+  <div class="max-w-sm mx-auto bg-white shadow pt-6 pb-4 px-6">
+      <Form @submit="signUp" :validation-schema="schema">
+        
+        <Field name="id" type="id" v-slot="{ field, errorMessage, meta }">
+          <div class="field">
+            <label for="id" class="block text-sm font-medium text-slate-700 text-left">아이디</label>
+            <div class="mt-1">
+              <input class="form-control" v-model="user.userid" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
+              <span class="errMsg block pt-2 pl-1 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
+            </div>
+          </div>
+        </Field>
+
+        <Field name="name" type="name" v-slot="{ field, errorMessage, meta }">
+          <div class="field mt-3">
+            <label for="name" class="block text-sm font-medium text-slate-700 text-left">이름</label>
+            <div class="mt-1">
+              <input class="form-control" v-model="user.name" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
+              <span class="errMsg block pt-2 pl-1 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
+            </div>
+          </div>
+        </Field>
+
+        <Field name="password" type="password" v-slot="{ field, errorMessage, meta }">
+          <div class="field mt-3 mb-3">
+            <label for="pwd" class="block text-sm font-medium text-slate-700 text-left">비밀번호</label>
+            <div class="mt-1">
+              <input type="password" class="form-control" v-model="user.password" v-bind="field" :class="{ 'is-invalid': !meta.valid && errorMessage }"/>
+              <span class="errMsg block pt-2 pl-1 text-[13px] text-left" v-if="errorMessage && !meta.valid">{{ errorMessage }}</span>
+            </div>
+          </div>
+        </Field>
+
+        <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+          가입하기
+        </button>
+
       </Form>
     </div>
+</div><div class="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div></div></div>
   </template>
 
 <script>
 
 import axios from 'axios'
+
+import { Form, Field } from 'vee-validate';
+import { object, string } from 'yup';
 
 export default {
 
@@ -34,7 +62,7 @@ export default {
       .then((res) => {
         if (res.data.success == true) {
           alert(res.data.message)
-          this.$router.push("/bodLogin")
+          this.$router.push("/DataDb")
         }
         if (res.data.success == false) {
           alert(res.data.message)
@@ -50,7 +78,20 @@ export default {
         password: ""
       },
     }
+  }, 
+  computed: {
+    schema() {
+      return object({
+        id: string().required('아이디를 입력해주세요.'),
+        name: string().required('이름을 입력해주세요.'),
+        password: string().required('비밀번호를 입력해주세요.').matches(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[#?!@$%^&*-])(?=.{8,})/,'영문자, 숫자, 특수문자를 조합하여 최소 8자리를 입력해주세요.')
+      });
+    },
   },
+  components: {
+    Form,
+    Field
+  }, 
 }
 
 </script>
